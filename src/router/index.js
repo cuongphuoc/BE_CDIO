f9aa358e2d0f25d76d32785eb5297d06b2cca134
cuongphuoc <cuongphuoc06@gmail.com>
2024-04-25 14:10:49 +0700

    lan 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 const khachhang = require("./khachhangRoter"); // Import the routes related to "khachhang"
-
+const datlich=require("./DatLichrouter");
 // Define a function called route that takes an Express app instance as an argument
 function route(app){
     // Mount the "khachhang" routes under the "/khachhang" path
+
     app.use("/khachhang", khachhang);
+    app.use("/datlich",datlich);
 }
 
 // Export the route function
